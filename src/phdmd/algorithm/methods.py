99548d0d0c48d323e6118eb890aba4d2c9469aec
jrettberg <johannes.rettberg@itm.uni-stuttgart.de>
2024-07-04 15:55:59 +0200

    add new cvx methods

--- src/phdmd/algorithm/methods.py
+++ src/phdmd/algorithm/methods.py
@@ -1,14 +1,19 @@
 from phdmd.algorithm.dmd import iodmd, operator_inference
 from phdmd.algorithm.phdmd import phdmd
 from phdmd.algorithm.phdmdh import phdmdh
+from phdmd.algorithm.cvxabcdph import cvxabcdph
+from phdmd.algorithm.cvxabcdpr import cvxabcdpr
+from phdmd.algorithm.cvxph import cvxph
 from phdmd.utils.system import to_lti, to_phlti
 
 from matplotlib import pyplot as plt
+
 
 class Method:
     """
     Base class for all methods.
     """
+
     def __init__(self, name):
         self.name = name
 
@@ -18,7 +23,7 @@ class Method:
 
 class IODMDMethod(Method):
     def __init__(self):
-        super().__init__('DMD')
+        super().__init__("DMD")
 
     def __call__(self, X, Y, U, delta_t, H):
         n = X.shape[0]
@@ -29,7 +34,7 @@ class IODMDMethod(Method):
 
 class OIMethod(Method):
     def __init__(self):
-        super().__init__('OI')
+        super().__init__("OI")
 
     def __call__(self, X, Y, U, delta_t, H):
         n = X.shape[0]
@@ -41,24 +46,52 @@ class OIMethod(Method):
 
 class PHDMDMethod(Method):
     def __init__(self):
-        super().__init__('pHDMD')
+        super().__init__("pHDMD")
 
-    def __call__(self, X, Y, U, delta_t, H, dXdt=None, use_Berlin = None, Q=None):
+    def __call__(
+        self,
+        X,
+        Y,
+        U,
+        delta_t,
+        H=None,
+        dXdt=None,
+        use_Berlin=None,
+        Q=None,
+        use_cvx=False,
+        J_known=None,
+    ):
         n = X.shape[0]
-        J, R, e = phdmd(X, Y, U, delta_t=delta_t, H=H, dXdt=dXdt)
+        J, R, e = phdmd(
+            X, Y, U, delta_t=delta_t, H=H, dXdt=dXdt, use_cvx=use_cvx, J_known=J_known
+        )
         phlti = to_phlti(J, R, n=n, E=H, no_feedtrough=True)
 
         return phlti
 
+
 class PHDMDHMethod(Method):
     def __init__(self):
-        super().__init__('pHDMDH')
+        super().__init__("pHDMDH")
 
-    def __call__(self, X, Y, U, delta_t, use_Berlin, H=None, Q=None):
+    def __call__(
+        self, X, Y, U, delta_t, use_Berlin, H=None, Q=None, use_cvx=False, J_known=None
+    ):
         # H not needed but used to be in accordance with other methods calling
         n = X.shape[0]
         max_iter = 1000
-        J, R, H, Q, e = phdmdh(X, Y, U, H0=H, Q0=Q, delta_t=delta_t, use_Berlin=use_Berlin, max_iter=max_iter)
+        J, R, H, Q, e = phdmdh(
+            X,
+            Y,
+            U,
+            H0=H,
+            Q0=Q,
+            delta_t=delta_t,
+            use_Berlin=use_Berlin,
+            max_iter=max_iter,
+            use_cvx=use_cvx,
+            J_known=J_known,
+        )
         phlti = to_phlti(J, R, n=n, E=H, Q=Q, no_feedtrough=True)
 
         plot_error_over_iter = True
@@ -69,6 +102,95 @@ class PHDMDHMethod(Method):
             plt.xlabel("Iterations")
             plt.ylabel("Rel. Fro norm error")
             plt.savefig(f"error_fro_Bform{use_Berlin}_iter{max_iter}.png")
+
+        return phlti
+
+
+class CVXABCDPHMethod(Method):
+    def __init__(self):
+        super().__init__("CVXABCD")
+
+    def __call__(
+        self,
+        X,
+        Y,
+        U,
+        delta_t,
+        use_Berlin,
+        H=None,
+        Q=None,
+        use_cvx=False,
+        J_known=None,
+        constraint_type="no",
+    ):
+        max_iter = None
+        phlti = cvxabcdph(
+            X,
+            Y,
+            U,
+            # dXdt=dXdt,
+            delta_t=delta_t,
+            # delta=delta,
+            constraint_type=constraint_type,  # "KYP" | "no" | "nsd" | "nsd"):
+        )
+
+        return phlti
+
+class CVXABCDPRMethod(Method):
+    def __init__(self):
+        super().__init__("CVXABCDPR")
+    
+    def __call__(        self,
+        X,
+        Y,
+        U,
+        delta_t,
+        use_Berlin,
+        H=None,
+        Q=None,
+        use_cvx=False,
+        J_known=None,
+        constraint_type="no",
+        **kwargs):
+
+        if "gillis_options" in kwargs:
+            gillis_options = kwargs["gillis_options"]
+        else:
+            gillis_options = None
+        phlti = cvxabcdpr(
+            X,
+            Y,
+            U,
+            # dXdt=dXdt,
+            delta_t=delta_t,
+            # delta=delta,
+            constraint_type=constraint_type,  # "KYP" | "no" | "nsd" | "nsd"):
+            gillis_options = gillis_options,
+        )
+
+        return phlti
+
+
+class CVXPHMethod(Method):
+    def __init__(self):
+        super().__init__("CVXPH")
+
+    def __call__(
+        self, X, Y, U, delta_t, use_Berlin, H=None, Q=None, use_cvx=False, J_known=None
+    ):
+        max_iter = None
+        phlti = cvxph(
+            X,
+            Y,
+            U,
+            H0=H,
+            Q0=Q,
+            delta_t=delta_t,
+            use_Berlin=use_Berlin,
+            max_iter=max_iter,
+            use_cvx=use_cvx,
+            J_known=J_known,
+        )
 
         return phlti
 
