145affcd1d41bbd9d70c8fbabb62c9e9e90eb2b6
jrettberg <johannes.rettberg@itm.uni-stuttgart.de>
2024-07-04 16:02:15 +0200

    include new variant

--- src/phdmd/main.py
+++ src/phdmd/main.py
@@ -5,12 +5,21 @@ import numpy as np
 from phdmd import config
 from phdmd.data.generate import generate
 from phdmd.evaluation.evaluation import evaluate
+from pymor.algorithms.to_matrix import to_matrix
 from phdmd.linalg.definiteness import project_spsd, project_spd
-from phdmd.utils.preprocess_data import get_Riccati_transform, get_initial_energy_matrix, perturb_initial_energy_matrix
+from phdmd.utils.preprocess_data import (
+    get_Riccati_transform,
+    get_initial_energy_matrix,
+    perturb_initial_energy_matrix,
+)
+from phdmd.utils.postprocess_data import compare_matrices
+from phdmd.algorithm.methods import CVXABCDPRMethod
+
 
 import cProfile
 import pstats
 from pymor.models.iosys import PHLTIModel, LTIModel
+
 
 def main():
     logging.basicConfig()
@@ -26,11 +35,11 @@ def main():
     experiments = config.experiments
 
     for exp in experiments:
-        logging.info(f'Experiment: {exp.name}')
+        logging.info(f"Experiment: {exp.name}")
         lti_dict = {}
 
         if exp.use_Riccatti_transform:
-            T = get_Riccati_transform(exp)
+            T = get_Riccati_transform(exp.fom)
 
         n = exp.fom.order
         use_Berlin = exp.use_Berlin
@@ -38,18 +47,23 @@ def main():
         H, Q = get_initial_energy_matrix(exp)
 
         if exp.perturb_energy_matrix:
-            H,Q = perturb_initial_energy_matrix(exp,H,Q)             
+            H, Q = perturb_initial_energy_matrix(exp, H, Q)
 
-        logging.info(f'State dimension n = {n}')
-        logging.info(f'Step size delta = {exp.delta:.2e}')
-        lti_dict['Original'] = exp.fom
+        if exp.use_known_J:
+            assert exp.use_cvx
+            J_known = exp.J
+        else:
+            J_known = None
+
+        logging.info(f"State dimension n = {n}")
+        logging.info(f"Step size delta = {exp.delta:.2e}")
+        lti_dict["Original"] = exp.fom
 
         # Generate/Load training data
         X_train, Y_train, U_train = generate(exp)
 
-
         if exp.use_Riccatti_transform:
-            X_train = T@X_train
+            X_train = T @ X_train
 
         # Plot training input, analogously output or state data
         # plot(exp.T, U_train, label='$u$', ylabel='Input', xlabel='Time (s)',
@@ -57,25 +71,43 @@ def main():
 
         # Perform methods
         for method in exp.methods:
-            lti = method(X_train, Y_train, U_train, exp.delta, use_Berlin, H=H, Q=Q)
+            if isinstance(method, CVXABCDPRMethod):
+                add_method_inputs = {"gillis_options": exp.gillis_options,
+                                     "constraint_type": exp.constraint_type}
+            lti = method(
+                X_train,
+                Y_train,
+                U_train,
+                exp.delta,
+                use_Berlin=use_Berlin,
+                H=H,
+                Q=Q,
+                use_cvx=exp.use_cvx,
+                J_known=J_known,
+                **add_method_inputs,
+            )
             lti_dict[method.name] = lti
 
+        latex_output = True
+        compare_matrices(exp, lti, latex_output=latex_output)
+
         # Evaluation
-        logging.info('Evaluate')
+        logging.info("Evaluate")
         evaluate(exp, lti_dict, compute_hinf=False)
 
-def profile_run(code_name_string='main()'):
+
+def profile_run(code_name_string="main()"):
     prof = cProfile.Profile()
     prof.run(code_name_string)
-    prof.sort_stats('cumtime')
-    prof.dump_stats(f'profile_output_{code_name_string}.prof')
+    prof.sort_stats("cumtime")
+    prof.dump_stats(f"profile_output_{code_name_string}.prof")
 
-    stream = open(f'profile_output_{code_name_string}.txt', 'w')
-    stats = pstats.Stats(f'profile_output_{code_name_string}.prof', stream=stream)
-    stats.sort_stats('cumtime')
+    stream = open(f"profile_output_{code_name_string}.txt", "w")
+    stats = pstats.Stats(f"profile_output_{code_name_string}.prof", stream=stream)
+    stats.sort_stats("cumtime")
     stats.print_stats()
 
 
 if __name__ == "__main__":
-    profile_run('main()')
-    print('debug stop')
+    profile_run("main()")
+    print("debug stop")
